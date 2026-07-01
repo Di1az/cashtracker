94c32353244a1c97337d24499f8fcfc66edc6554
diaz <128860188+Di1az@users.noreply.github.com>
2026-07-01 11:31:24 -0600

    feat: allow users to view their budgets

--- app/Models/Budget.php
+++ app/Models/Budget.php
@@ -2,15 +2,31 @@
 
 namespace App\Models;
 
+use App\BudgetType;
 use Illuminate\Database\Eloquent\Model;
 
 class Budget extends Model
 {
     protected $fillable = ['name', 'amount', 'type', 'user_id'];
 
+    //Convertir el enum a string para poder compararlos e imprimirlos
+    protected $casts = [
+        'type' => BudgetType::class
+    ];
+
     public function user()
     {
         $this->belongsTo(User::class);
+    }
+
+    public function isGeneral() : bool
+    {
+        return $this->type === BudgetType::GENERAL; 
+    }
+
+    public function isGoal() : bool
+    {
+        return $this->type === BudgetType::GOAL;
     }
 
 }
